5258b2c5be99961e6e6cb627164b3610aa4ced93
kyleyiyang <kyleyiyang@gmail.com>
2020-10-09 08:50:48 -0400

    Update sortArrayByParityII.java

--- sortArrayByParityII.java
+++ sortArrayByParityII.java
@@ -5,10 +5,12 @@ public class HelloWorld{
         int[] A = {4,2,5,7,6,9,11,12,13,15,17,16,18,20};
         int j=0;
         for (int i=0;i<A.length;i++) {
-            while (j%2==A[j]%2 && j<A.length-1) {
+            while (j%2==A[j]%2 && j<A.length) {
                 j++;
             }
-            
+            if (i<=j) {
+                i=j+1;
+            }
             if (i%2!=A[i]%2) {
                 int temp = A[j];
                 if (i!=j && A[i]%2!=A[j]%2) {
